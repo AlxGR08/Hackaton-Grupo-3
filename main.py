89aef8c18c0a44370968f6c142e23c4d1db071ee
Marco Apolinario <marco.apolinario@utec.edu.pe>
2024-08-31 16:06:31 -0500

    Se añadio la funcion de multiplicación para tener una opción más para el usuario

--- main.py
+++ main.py
@@ -1,1 +1,9 @@
+def multiplicacion(a, b):
+    return a * b
 
+
+a = float(input("Ingrese el primer número: "))
+b = float(input("Ingrese el segundo número: "))
+    
+resultado = a * b
+print(f"El resultado de {a} * {b} es: {resultado}")
