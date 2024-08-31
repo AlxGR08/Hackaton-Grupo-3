f92c04de53506d5e118192f3aff93706e7a32b83
Marco Apolinario <marco.apolinario@utec.edu.pe>
2024-08-31 17:05:25 -0500

    Mi presentacion

--- main.py
+++ main.py
@@ -1,1 +1,12 @@
+def division():
+    a = float(input("Ingrese el primer número (dividendo): "))
+    b = float(input("Ingrese el segundo número (divisor): "))
+
+    if b == 0:
+        print("Error: No se puede dividir entre cero.")
+    else:
+        resultado = a / b
+        print(f"El resultado de {a} dividido por {b} es: {resultado}")
+
+division()
 
